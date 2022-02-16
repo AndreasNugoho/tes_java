9edc091acccee73b330755b531da5d7d9ffbf679
AndreasNugroho <andreas.nugroho@ti.ukdw.ac.id>
2022-02-16 14:42:52 +0700

    coba

---
+++ src/lampu.java
@@ -0,0 +1,19 @@
+public class lampu {
+
+    String warna;
+
+    public void nyala(){
+
+    }
+
+    public void mati() {
+
+    }
+    public void gantiWarna(String warnaBaru){
+        warna = warnaBaru;
+    }
+    public String warnaSekarang(){
+        return warna;
+    }
+
+}
